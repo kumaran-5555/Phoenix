b9ed334786e1f42ea4908f2a842abc5a4c5736a1
kanika1712 <kanika1712@gmail.com>
2015-08-16 15:26:28 +0530

    Added missing fields in existing models

--- PhoenixDev/PhoenixDev/PhoenixWebService/models.py
+++ PhoenixDev/PhoenixDev/PhoenixWebService/models.py
@@ -28,7 +28,7 @@ class Products(models.Model):
     productName = models.CharField(max_length=200)
     productMinPrice = models.IntegerField()
     productMaxPrice = models.IntegerField()
-
+    productBrandName = models.ForeignKey('ProductBrands')
 
 class ProductBrands(models.Model):
     brandName = models.CharField(max_length=200)
@@ -106,6 +106,9 @@ class Users(models.Model):
     userName = models.CharField(max_length=100)
     userPrimaryPhone = models.CharField(max_length=11)
     userMailId = models.CharField(max_length=100)
+    # For completion sake, these fields can be optional for user.
+    userCityName = models.CharField(max_length=50)
+    userAddress = models.CharField(max_length=300)
     # sha512
     userPasswordHash = models.CharField(max_length=512)
     userSignupTime = models.DateTimeField(auto_now_add=True)
